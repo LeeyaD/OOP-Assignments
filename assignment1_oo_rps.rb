6573a3b6292e8eeb774c4c74c410233fad948064
Leeya Davis <leeya463@gmail.com>
2020-04-29 18:10:02 -0400

    oop skeleton with orchestration engine

--- assignment1_oo_rps.rb
+++ assignment1_oo_rps.rb
@@ -25,3 +25,22 @@ end
 def compare(move1, move2)
 
 end
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new
+  end
+
+  def play
+    display_welcome_message
+    human_choose_move
+    computer_choose_move
+    display_winner
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
